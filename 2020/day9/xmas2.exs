970ce316f8e46266d18d753532744f1693c64703
ramonpin <ramon.pin@gmail.com>
2022-01-04 19:37:57 +0100

    fix: Better XMAS.process definition

--- 2020/day9/xmas2.exs
+++ 2020/day9/xmas2.exs
@@ -8,28 +8,29 @@ defmodule XMAS do
   end
 
   def run(file, n) do
-    case process(n, load_data(file), [], 0) do
+    case process(n, load_data(file)) do
       :not_found -> :not_found
       list -> list |> Enum.min_max() |> Tuple.sum()
     end
   end
 
-  def process(n, [], _, _),
+  def process(n, numbers),
+    do: process(n, numbers, [], 0)
+
+  defp process(n, [], _, _),
     do: :not_found
 
-  def process(n, [m | rest], [], 0),
+  defp process(n, _, cache, n),
+    do: cache
+
+  defp process(n, [m | rest], [], 0),
     do: process(n, rest, [m], m)
 
-  def process(n, [m | rest] = numbers, [_ | crest] = cache, sum) do
-    cond do
-      m + sum == n ->
-        cache ++ [m]
-
-      m + sum < n ->
-        process(n, rest, cache ++ [m], m + sum)
-
-      true ->
-        process(n, crest ++ numbers, [], 0)
+  defp process(n, [m | rest] = numbers, [_ | crest] = cache, sum) do
+    if m + sum <= n do
+      process(n, rest, cache ++ [m], m + sum)
+    else
+      process(n, crest ++ numbers, [], 0)
     end
   end
 end
